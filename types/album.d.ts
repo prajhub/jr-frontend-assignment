f083ce26e882b5bd6ee8a9754ca7cc1ef91fd1c3
prajhub <pzlmagar@gmail.com>
2023-07-02 23:12:04 +0545

    Final updates.

--- types/album.d.ts
+++ types/album.d.ts
@@ -33,6 +33,35 @@ export interface Album {
     };
   };
   release_date: string;
+  id: string
+  tracks: {
+    href: string;
+    items: Track[];
+  };
+ 
+}
+
+export interface Track {
+  artists: {
+    [index: number]: {
+      name: string;
+    };
+  };
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_urls: {
+    spotify: string;
+  };
+  href: string;
+  id: string;
+  is_local: boolean;
+  name: string;
+  preview_url: string;
+  track_number: number;
+  type: string;
+  uri: string;
 }
 
 export type AlbumArray = Album[];
